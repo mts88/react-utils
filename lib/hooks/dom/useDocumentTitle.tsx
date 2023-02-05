6e3debd74de7125cf3eb51386c9f8c29f503ccb5
MTS <fsca.motisi@gmail.com>
2023-02-05 16:50:21 +0100

    feat: initial config
    Initial configuration and first hooks

---
+++ lib/hooks/dom/useDocumentTitle.tsx
@@ -0,0 +1,44 @@
+type IUseDocumentTitle = {
+  /**
+   * Update current DOM title
+   */
+  updateDocumentTitle: (newTitle: string) => void;
+};
+
+/**
+ * Set DOM title, and return methods to update it.
+ *
+ * @param title *Optional* - Title to apply
+ *
+ * @returns Methods to update title. See {@link IUseDocumentTitle}.
+ *
+ * @example
+ * ```
+ *  const MyComponent = () => {
+ *
+ *      const { updateDocumentTitle } = useDocumentTitle('Initial title')
+ *      // .... other params
+ *
+ *      useEffect(() => {
+ *          updateDocumentTitle('Update my title')
+ *          return () => {
+ *              updateDocumentTitle('Initial title')
+ *          }
+ *      }, [...deps])
+ *  }
+ *
+ * ```
+ */
+export default function useDocumentTitle(title?: string): IUseDocumentTitle {
+  if (title) {
+    document.title = title;
+  }
+
+  const updateDocumentTitle = (newTitle: string): void => {
+    document.title = newTitle;
+  };
+
+  return {
+    updateDocumentTitle,
+  };
+}
